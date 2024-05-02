ab7a70cfe31e5a2498c3c48ff935918ac58c5560
Arseny <20096ninja@gmail.com>
2024-05-02 19:16:18 +0300

    added plane

--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-width = 200
-height = 100
-image = np.zeros((height, width, 3))
-for i in range(height):
-    for j in range(width):
-        image[i][j] = [
-            i / height,
-            j / width,
-            .2
-        ]
+
+class Ray:
+    def __init__(self, a: np.array, b: np.array):
+        self.A = a
+        self.B = b
+
+    def origin(self):
+        return self.A
+
+    def direction(self):
+        return self.B
+
+    def point_at_parameter(self, t: float):
+        return self.A + t * self.B
+
+
+def color(ray: Ray):
+    unit_direction = ray.direction() / np.linalg.norm(ray.direction())
+    t = 0.5 * (unit_direction[1] + 1.0)
+    return (1.0 - t) * np.array([1.0, 1.0, 1.0]) + t * np.array([0.5, 0.7, 1.0])
+
+
+nx = 200
+ny = 100
+
+lower_left_corner = np.array([-2.0, -1.0, -1.0])
+horizontal = np.array([4.0, 0.0, 0.0])
+vertical = np.array([0.0, 2.0, 0.0])
+
+origin = np.array([0.0, 0.0, 0.0])
+
+image = np.zeros((ny, nx, 3))
+for j in range(ny):
+    for i in range(nx):
+        u = i / nx
+        v = (ny - j) / ny
+        r = Ray(origin, lower_left_corner + u * horizontal + v * vertical)
+        col = color(r)
+        image[j, i] = col
 
 plt.imsave("image.png", image)
