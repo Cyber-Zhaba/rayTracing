99669f91dbd8c252ef39c5d33abbe1511d690ff4
Arseny <20096ninja@gmail.com>
2024-05-04 11:17:43 +0300

    created cam class and added antialiasing

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+from random import random
+
 from matplotlib import pyplot as plt
 from tqdm import trange
 
 from hittable import *
+
+
+class Camera:
+    def __init__(self):
+        self.lower_left_corner = np.array([-2.0, -1.0, -1.0])
+        self.horizontal = np.array([4.0, 0.0, 0.0])
+        self.vertical = np.array([0.0, 2.0, 0.0])
+        self.origin = np.array([0.0, 0.0, 0.0])
+
+    def get_ray(self, u, v):
+        return Ray(self.origin, self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin)
 
 
 def color(ray: Ray, world: Hittable):
@@ -17,22 +30,24 @@ def color(ray: Ray, world: Hittable):
 def render():
     nx = 200
     ny = 100
+    ns = 100
 
-    lower_left_corner = np.array([-2.0, -1.0, -1.0])
-    horizontal = np.array([4.0, 0.0, 0.0])
-    vertical = np.array([0.0, 2.0, 0.0])
+    cam = Camera()
 
-    origin = np.array([0.0, 0.0, 0.0])
     image = np.zeros((ny, nx, 3))
 
     world = Hittable([Sphere(np.array([0, 0, -1]), 0.5), Sphere(np.array([0, -100.5, -1]), 100)])
 
     for j in trange(ny):
         for i in range(nx):
-            u = i / nx
-            v = (ny - j) / ny
-            r = Ray(origin, lower_left_corner + u * horizontal + v * vertical)
-            col = color(r, world)
+            col = np.array([0, 0, 0])
+            for s in range(ns):
+                u = (i + random() / 2) / nx
+                v = (ny - j + random() / 2) / ny
+                r = cam.get_ray(u, v)
+                p = r.point_at_parameter(2.0)
+                col = col + color(r, world)
+            col /= ns
             image[j, i] = col
 
     plt.imsave("image.png", image)
